c964014ff999cfc714a9b1c157863adb803af998
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-25 21:34:59 -0500

    Final commit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { global, reset } from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <Global styles={reset} />
     <Global styles={global} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </>
 );
 
